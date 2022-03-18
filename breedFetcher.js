9fbd2c17fd5fd41e52cd425c04b017ae3509fe5c
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-18 00:27:04 +0000

    updated to search using cl arg

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,15 @@
-const require = require('request');
+// searching by breed: https://docs.thecatapi.com/api-reference/breeds/breeds-search
+
+const request = require('request');
+
+const breed = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+request(url, (err, response, body) => {
+  if (err) console.error(err);
+  else {
+    const data = JSON.parse(body);
+    if (data.length === 0) console.log(`Error: Specified breed ${breed} not found.`);
+    else console.log(data);
+  }
+});
